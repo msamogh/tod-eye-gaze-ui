e319b434e422d106ee165cc6da89da932362741f
Amogh Mannekote <msamogh@gmail.com>
2022-03-14 16:07:47 -0400

    Working eye tracking

--- app.py
+++ app.py
@@ -64,18 +64,61 @@ def get_last_line(path):
         return last_line
 
 
+def get_last_n_lines(file_name, N):
+    # Create an empty list to keep the track of last N lines
+    list_of_lines = []
+    # Open file for reading in binary mode
+    with open(file_name, "rb") as read_obj:
+        # Move the cursor to the end of the file
+        read_obj.seek(0, os.SEEK_END)
+        # Create a buffer to keep the last read line
+        buffer = bytearray()
+        # Get the current position of pointer i.e eof
+        pointer_location = read_obj.tell()
+        # Loop till pointer reaches the top of the file
+        while pointer_location >= 0:
+            # Move the file pointer to the location pointed by pointer_location
+            read_obj.seek(pointer_location)
+            # Shift pointer location by -1
+            pointer_location = pointer_location - 1
+            # read that byte / character
+            new_byte = read_obj.read(1)
+            # If the read byte is new line character then it means one line is read
+            if new_byte == b"\n":
+                # Save the line in list of lines
+                list_of_lines.append(buffer.decode()[::-1])
+                # If the size of list reaches N, then return the reversed list
+                if len(list_of_lines) == N:
+                    return list(reversed(list_of_lines))
+                # Reinitialize the byte array to save next line
+                buffer = bytearray()
+            else:
+                # If last read character is not eol then add it in buffer
+                buffer.extend(new_byte)
+        # As file is read completely, if there is still data in buffer, then its first line.
+        if len(buffer) > 0:
+            list_of_lines.append(buffer.decode()[::-1])
+    # return the reversed list
+    return list(reversed(list_of_lines))
+
+
 @app.route("/latest_gaze_coords", methods=["POST"])
 def latest_gaze_coords():
     while True:
         try:
-            last_line = get_last_line(GAZE_STREAM_PATH)
-            latest_log = json.loads(last_line)["values"]["frame"]["avg"]
-            # print(f"Latest log: {latest_log}")
-            break
+            last_lines = get_last_n_lines(GAZE_STREAM_PATH, 5)
+            for line in last_lines[::-1]:
+                if line.strip() == "":
+                    continue
+                line = json.loads(line)
+                print(line)
+                if line["category"] == "tracker":
+                    response = {"coords": line["values"]["frame"]["avg"]}
+                    print(response)
+                    return jsonify(response)
         except Exception as e:
             print(e)
             continue
-    return jsonify(latest_log)
 
 
 @app.route("/telemetry", methods=["POST"])
