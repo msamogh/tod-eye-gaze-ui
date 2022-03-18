beaa1a35ed0ce2d597aa837a110cbe569fd87edd
Ashwin Gopinath <ashwing22@gmail.com>
2022-03-17 21:57:52 -0400

    Added question highlight for both the datasets

--- constants.py
+++ constants.py
@@ -3,12 +3,90 @@ GAZE_PATH_FOLDER = "gaze-paths"
 
 MULTIWOZ_PRETTY_SLOTS = {
     "hotel": {
+        "area": "Direction of location",
         "book day": "Day of Booking",
         "book people": "Number of people to book for",
         "book stay": "Duration of stay",
+        "internet": "Availability of internet",
+        "name" : "Name of the hotel",
+        "parking": "Avaliability of Parking",
+        "pricerange": "Price Range of the hotel",
+        "stars": "Hotel Star Rating",
+        "type": "Type of Hotel"
+    },
+    "hospital": {
+        "department": "Name of the department"
+    },
+    "restaurant": {
+        "area": "Direction of the location of the restaurant",
+        "book day": "Day of restaurant booking",
+        "book people" : "Number of people to book the restaurant",
+        "book time": "Time of booking of the restaurant",
+        "food": "Food cuisine of the restaurant",
+        "name": "Name of the restaurant",
+        "pricerange": "Range of price of the restaurant"
+    },
+    "attraction": {
+        "area": "Direction of location of the attraction",
+        "name": "Name of the attraction",
+        "type": "Type of the attraction"
     }
 }
 
 STAR_PRETTY_SLOTS = {
-
+    "apartment": {
+        "Name": "Name of the apartment",
+        "Level": "Level/Floor of the apartment",
+        "MaxLevel": "Max Level/Floor of the apartment",
+        "HasBalcony": "Apartment has balcony",
+        "BalconySide": "Direction of the balcony",
+        "HasElevator": "Apartment has elevator",
+        "NumRooms": "Number of rooms in the apartment",
+        "FloorSquareMeters": "Square Metres of the floor",
+        "Price": "Price of the Apartment",
+        "NearbyPOIs": "Nearby POIs to the apartment"
+    },
+    "bank": {
+        "BankName": "Name of the Bank",
+        "BankBalance": "Balance amount in the bank"
+    },
+    "hotel": {
+        "Name": "Name of the hotel",
+        "Cost": "Cost Category of the hotel",
+        "TakesReservations": "Are Hotel Reservations allowed",
+        "Service": "Is Hotel Service Available",
+        "AverageRating": "Average Star Rating of the Hotel",
+        "ServiceStartHour": "Hotel Service Start hour",
+        "ServiceStopHour": "Hotel Service Stop hour",
+        "Location": "Location of the hotel"
+    },
+    "movie": {
+        "Name": "Name of the movie",
+        "Actors": "Name the actors of the movie",
+        "Director": "Name the director of the movie",
+        "DurationMinutes": "Duration of the movie in minutes",
+        "Genre": "Name the movie genre",
+        "Platforms": "Name the platform the movie is available in"
+    },
+    "plane": {
+        "DepartureCity": "Departure city of the plane",
+        "ArrivalCity": "Arrival city of the plane",
+        "Price": "Price of travel on the plane",
+        "Date": "Data of travel on the plane",
+        "ArrivalTime": "Time of arrival of the plane",
+        "Class": "Class of travel on the plane",
+        "DurationHours": "Duration of travel on plane in hours",
+        "Airline": "Airline company of the plane",
+        "Name": "Plane number"
+    },
+    "ride": {
+        "Price": "Price of the ride",
+        "AllowsChanges": "Are ride changes allowed?",
+        "MinutesTillPickup": "Ride pickup in minutes",
+        "ServiceProvider": "Ride service provider",
+        "DriverName": "Driver name of the ride",
+        "CarModel": "Car model of the ride",
+        "LicensePlate": "License plate number of the ride",
+        "id": "Ride number"
+    }
 }
